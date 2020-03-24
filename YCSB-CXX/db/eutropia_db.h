b76156064d3a9836376b382b5365e0a9a9712bbc
gxanth <gxanth@ics.forth.gr>
2020-03-24 22:05:40 +0200

    Variable size leaves

--- YCSB-CXX/db/eutropia_db.h
+++ YCSB-CXX/db/eutropia_db.h
@@ -11,8 +11,6 @@
 #ifndef YCSB_C_EUTROPIA_DB_H_
 #define YCSB_C_EUTROPIA_DB_H_
 
-
-
 #include <iostream>
 #include <string>
 #include <mutex>
@@ -21,7 +19,6 @@
 #include <functional>
 #include <sstream>
 #include <iterator>
-
 
 #include <inttypes.h>
 #include <stdlib.h>
@@ -44,130 +41,139 @@ extern "C" {
 using std::cout;
 using std::endl;
 
-namespace ycsbc {
-  class EutropiaDB : public YCSBDB {
-  private:
-    int db_num;
-    int field_count;
-    std::vector<db_handle *> dbs;
+namespace ycsbc
+{
+class EutropiaDB : public YCSBDB {
+    private:
+	int db_num;
+	int field_count;
+	std::vector<db_handle *> dbs;
 
-  public:
-    EutropiaDB(int num, utils::Properties& props) : db_num(num), field_count(std::stoi(props.GetProperty(CoreWorkload::FIELD_COUNT_PROPERTY, CoreWorkload::FIELD_COUNT_DEFAULT))), dbs()
-    {
-      //const char *pathname = "/dev/dmap/dmap1";
-      const char *pathname = "/dev/nvme0n1";
-      int64_t size;
+    public:
+	EutropiaDB(int num, utils::Properties &props)
+		: db_num(num), field_count(std::stoi(props.GetProperty(CoreWorkload::FIELD_COUNT_PROPERTY,
+								       CoreWorkload::FIELD_COUNT_DEFAULT))),
+		  dbs()
+	{
+		//const char *pathname = "/dev/dmap/dmap1";
+		const char *pathname = "/dev/nvme0n1";
+		int64_t size;
 
-      int fd = open(pathname, O_RDONLY);
-      if(fd == -1){
-        perror("open");
-        exit(EXIT_FAILURE);
-      }
+		int fd = open(pathname, O_RDONLY);
+		if (fd == -1) {
+			perror("open");
+			exit(EXIT_FAILURE);
+		}
 
-      if(ioctl(fd, BLKGETSIZE64, &size) == -1)
-      {
-        perror("ioctl");
-        /*maybe we have a file?*/
-        printf("[%s:%s:%d] querying file size\n",__FILE__,__func__,__LINE__);
-        size = lseek(fd, 0, SEEK_END);
-        if(size == -1)
-        {
-          printf("[%s:%s:%d] failed to determine volume size exiting...\n",__FILE__,__func__,__LINE__);
-          perror("ioctl");
-          exit(EXIT_FAILURE);
-        }
-      }
+		if (ioctl(fd, BLKGETSIZE64, &size) == -1) {
+			perror("ioctl");
+			/*maybe we have a file?*/
+			printf("[%s:%s:%d] querying file size\n", __FILE__, __func__, __LINE__);
+			size = lseek(fd, 0, SEEK_END);
+			if (size == -1) {
+				printf("[%s:%s:%d] failed to determine volume size exiting...\n", __FILE__, __func__,
+				       __LINE__);
+				perror("ioctl");
+				exit(EXIT_FAILURE);
+			}
+		}
 
-      close(fd);
+		close(fd);
 
-      for(int i = 0; i < db_num; ++i){
-        std::string db_name = "data" + std::to_string(i) + ".dat";
-        db_handle * db = db_open((char *)pathname, 0, size, (char *)db_name.c_str(),CREATE_DB);
-        dbs.push_back(db);
-      }
-    }
+		for (int i = 0; i < db_num; ++i) {
+			std::string db_name = "data" + std::to_string(i) + ".dat";
+			db_handle *db = db_open((char *)pathname, 0, size, (char *)db_name.c_str(), CREATE_DB);
+			dbs.push_back(db);
+		}
+	}
 
-    virtual ~EutropiaDB(){}
+	virtual ~EutropiaDB()
+	{
+	}
 
-  public:
-    void Init(){}
+    public:
+	void Init()
+	{
+	}
 
-    void Close(){
-      snapshot(dbs[0]->volume_desc);
-      //flush_volume(dbs[0]->volume_desc, SPILL_ALL_DBS_IMMEDIATELY);
-    }
+	void Close()
+	{
+		snapshot(dbs[0]->volume_desc);
+		//flush_volume(dbs[0]->volume_desc, SPILL_ALL_DBS_IMMEDIATELY);
+	}
 
-    int __read(int id, const std::string &table, const std::string &key, const std::vector<std::string> *fields, std::vector<KVPair> &result)
-    {
-      std::hash<std::string> hash_fn;
-      uint32_t db_id = hash_fn(key) % db_num;
-      std::map<std::string, std::string> vmap;
+	int __read(int id, const std::string &table, const std::string &key, const std::vector<std::string> *fields,
+		   std::vector<KVPair> &result)
+	{
+		std::hash<std::string> hash_fn;
+		uint32_t db_id = hash_fn(key) % db_num;
+		std::map<std::string, std::string> vmap;
 
-      char *val = (char *)find_key(dbs[db_id], (void *)key.c_str(), key.length());
-      if(val == NULL){
-        std::cout << "[1]cannot find : " << key << " in DB " << db_id << std::endl;
-        return 0;
-        exit(EXIT_FAILURE);
-      }
- //     return 0;
-//#if 0
-      if(*(int32_t *)val > 16000){
-        std::cout << "TOO LARGE VALUE SIZE IN READ!" << std::endl;
-        std::cout << "[" << *(int32_t *)val << "]" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-//#endif
-      std::string value(val + sizeof(int32_t), *(int32_t *)val);
+		char *val = (char *)find_key(dbs[db_id], (void *)key.c_str(), key.length());
+		if (val == NULL) {
+			std::cout << "[1]cannot find : " << key << " in DB " << db_id << std::endl;
+			return 0;
+			exit(EXIT_FAILURE);
+		}
+		//     return 0;
+		//#if 0
+		if (*(int32_t *)val > 16000) {
+			std::cout << "TOO LARGE VALUE SIZE IN READ!" << std::endl;
+			std::cout << "[" << *(int32_t *)val << "]" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		//#endif
+		std::string value(val + sizeof(int32_t), *(int32_t *)val);
 
-      std::vector<std::string> tokens;
-      boost::split(tokens, value, boost::is_any_of(" "));
+		std::vector<std::string> tokens;
+		boost::split(tokens, value, boost::is_any_of(" "));
 
-      int cnt = 0;
-      for(std::map<std::string, std::string>::size_type i = 0 ; i + 1 < tokens.size(); i += 2){
-        vmap.insert(std::pair<std::string, std::string>(tokens[i], tokens[i+1]));
-        ++cnt;
-      }
-//#if 0
-      if(cnt != field_count){
-        std::cout << "ERROR IN VALUE!" << std::endl;
-        std::cout << "[" << value << "]" << std::endl;
-        exit(EXIT_FAILURE);
-      }
-//#endif
+		int cnt = 0;
+		for (std::map<std::string, std::string>::size_type i = 0; i + 1 < tokens.size(); i += 2) {
+			vmap.insert(std::pair<std::string, std::string>(tokens[i], tokens[i + 1]));
+			++cnt;
+		}
+		//#if 0
+		if (cnt != field_count) {
+			std::cout << "ERROR IN VALUE!" << std::endl;
+			std::cout << "[" << value << "]" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		//#endif
 
-      for(auto f : *fields){
-        std::map<std::string, std::string>::iterator it = vmap.find(f);
-        if(it == vmap.end()){
-          std::cout << "[2]cannot find : " << f << " in DB " << db_id << std::endl;
-          return 0;
-          exit(EXIT_FAILURE);
-        }
+		for (auto f : *fields) {
+			std::map<std::string, std::string>::iterator it = vmap.find(f);
+			if (it == vmap.end()) {
+				std::cout << "[2]cannot find : " << f << " in DB " << db_id << std::endl;
+				return 0;
+				exit(EXIT_FAILURE);
+			}
 
-        KVPair k = std::make_pair(f, it->second);
-        result.push_back(k);
-      }
+			KVPair k = std::make_pair(f, it->second);
+			result.push_back(k);
+		}
 
-      return 0;
-    }
+		return 0;
+	}
 
-    int Read(int id, const std::string &table, const std::string &key, const std::vector<std::string> *fields, std::vector<KVPair> &result)
-    {
-      if(fields){
-        return __read(id, table, key, fields, result);
-      }else{
-        std::vector<std::string> __fields;
-        for(int i = 0; i < field_count; ++i)
-          __fields.push_back("field" + std::to_string(i));
-        return __read(id, table, key, &__fields, result);
-      }
+	int Read(int id, const std::string &table, const std::string &key, const std::vector<std::string> *fields,
+		 std::vector<KVPair> &result)
+	{
+		if (fields) {
+			return __read(id, table, key, fields, result);
+		} else {
+			std::vector<std::string> __fields;
+			for (int i = 0; i < field_count; ++i)
+				__fields.push_back("field" + std::to_string(i));
+			return __read(id, table, key, &__fields, result);
+		}
 
-      return 0;
-    }
+		return 0;
+	}
 
-    int Scan(int id, const std::string &table, const std::string &key, int len,
-       const std::vector<std::string> *fields,
-       std::vector<KVPair> &result)
-    {
+	int Scan(int id, const std::string &table, const std::string &key, int len,
+		 const std::vector<std::string> *fields, std::vector<KVPair> &result)
+	{
 #if 0
       char key_buf[512];
       int items = 0;
@@ -249,22 +255,21 @@ namespace ycsbc {
       closeScanner(&sh);
 #endif
 
-      return 0;
-    }
+		return 0;
+	}
 
-    int Update(int id, const std::string &table, const std::string &key, std::vector<KVPair> &values)
-    {
+	int Update(int id, const std::string &table, const std::string &key, std::vector<KVPair> &values)
+	{
+		if (field_count > 1) { // this results in read-modify-write. Maybe we should use merge operator here
+			std::hash<std::string> hash_fn;
+			uint32_t db_id = hash_fn(key) % db_num;
+			std::map<std::string, std::string> vmap;
 
-      if(field_count > 1){ // this results in read-modify-write. Maybe we should use merge operator here
-        std::hash<std::string> hash_fn;
-        uint32_t db_id = hash_fn(key) % db_num;
-        std::map<std::string, std::string> vmap;
-
-        char *val = (char *)find_key(dbs[db_id], (void *)key.c_str(), key.length());
-        if(val == NULL){
-          std::cout << "[1]cannot find : " << key << " in DB " << db_id << std::endl;
-          exit(EXIT_FAILURE);
-        }
+			char *val = (char *)find_key(dbs[db_id], (void *)key.c_str(), key.length());
+			if (val == NULL) {
+				std::cout << "[1]cannot find : " << key << " in DB " << db_id << std::endl;
+				exit(EXIT_FAILURE);
+			}
 #if 0
         if(*(int32_t *)val > 16000){
           std::cout << "TOO LARGE VALUE SIZE IN READ!" << std::endl;
@@ -272,16 +277,16 @@ namespace ycsbc {
           exit(EXIT_FAILURE);
         }
 #endif
-        std::string value(val + sizeof(int32_t), *(int32_t *)val);
+			std::string value(val + sizeof(int32_t), *(int32_t *)val);
 
-        std::vector<std::string> tokens;
-        boost::split(tokens, value, boost::is_any_of(" "));
+			std::vector<std::string> tokens;
+			boost::split(tokens, value, boost::is_any_of(" "));
 
-        int cnt = 0;
-        for(std::map<std::string, std::string>::size_type i = 0 ; i + 1 < tokens.size(); i += 2){
-          vmap.insert(std::pair<std::string, std::string>(tokens[i], tokens[i+1]));
-          ++cnt;
-        }
+			int cnt = 0;
+			for (std::map<std::string, std::string>::size_type i = 0; i + 1 < tokens.size(); i += 2) {
+				vmap.insert(std::pair<std::string, std::string>(tokens[i], tokens[i + 1]));
+				++cnt;
+			}
 #if 0
         if(cnt != field_count){
           std::cout << "ERROR IN VALUE!" << std::endl;
@@ -289,24 +294,24 @@ namespace ycsbc {
           exit(EXIT_FAILURE);
         }
 #endif
-        for(auto f : values){
-          std::map<std::string, std::string>::iterator it = vmap.find(f.first);
-          if(it == vmap.end()){
-            std::cout << "[2][UPDATE] Cannot find : " << f.first << " in DB " << db_id << std::endl;
-            exit(EXIT_FAILURE);
-          }
+			for (auto f : values) {
+				std::map<std::string, std::string>::iterator it = vmap.find(f.first);
+				if (it == vmap.end()) {
+					std::cout << "[2][UPDATE] Cannot find : " << f.first << " in DB " << db_id
+						  << std::endl;
+					exit(EXIT_FAILURE);
+				}
 
-          it->second = f.second;
-        }
+				it->second = f.second;
+			}
 
+			std::vector<KVPair> new_values;
+			for (std::map<std::string, std::string>::iterator it = vmap.begin(); it != vmap.end(); ++it) {
+				KVPair kv = std::make_pair(it->first, it->second);
+				new_values.push_back(kv);
+			}
 
-        std::vector<KVPair> new_values;
-        for(std::map<std::string, std::string>::iterator it = vmap.begin(); it != vmap.end(); ++it){
-          KVPair kv = std::make_pair(it->first, it->second);
-          new_values.push_back(kv);
-        }
-
-        return Insert(id, table, key, new_values);
+			return Insert(id, table, key, new_values);
 #if 0
         std::string new_value;
         for(auto v : vmap){
@@ -324,26 +329,26 @@ namespace ycsbc {
 
         memcpy(val + sizeof(int32_t), new_value.c_str(), *(int32_t *)val);
 #endif
-      }else{
-        return Insert(id, table, key, values);
-      }
-    }
+		} else {
+			return Insert(id, table, key, values);
+		}
+	}
 
-    int Insert(int id, const std::string &table, const std::string &key, std::vector<KVPair> &values)
-    {
-      std::hash<std::string> hash_fn;
-      uint32_t db_id = hash_fn(key) % db_num;
+	int Insert(int id, const std::string &table, const std::string &key, std::vector<KVPair> &values)
+	{
+		std::hash<std::string> hash_fn;
+		uint32_t db_id = hash_fn(key) % db_num;
 
-      std::string value;
-      int cnt = 0;
-      for(auto v : values){
-        value.append(v.first);
-        value.append(1, ' ');
-        value.append(v.second);
-        value.append(1, ' ');
-        ++cnt;
-      }
-      value.pop_back();
+		std::string value;
+		int cnt = 0;
+		for (auto v : values) {
+			value.append(v.first);
+			value.append(1, ' ');
+			value.append(v.second);
+			value.append(1, ' ');
+			++cnt;
+		}
+		value.pop_back();
 
 #if 0
       if(cnt != field_count){
@@ -353,26 +358,25 @@ namespace ycsbc {
       }
 #endif
 
-      insert_key_value(dbs[db_id], (void *)key.c_str(), (void *)value.c_str(), key.length(), value.size());
+		insert_key_value(dbs[db_id], (void *)key.c_str(), (void *)value.c_str(), key.length(), value.size());
 
-      return 0;
-    }
+		return 0;
+	}
 
-    int Delete(int id, const std::string &table,const std::string &key)
-    {
-      std::hash<std::string> hash_fn;
-      int8_t ret;
-      uint32_t db_id = hash_fn(key) % db_num;
+	int Delete(int id, const std::string &table, const std::string &key)
+	{
+		std::hash<std::string> hash_fn;
+		int8_t ret;
+		uint32_t db_id = hash_fn(key) % db_num;
 
-      ret = delete_key(dbs[db_id], (void *)key.c_str(), key.size());
-      if(ret != 0){
-        std::cerr << "I could not delete "<< key << std::endl;
-        return 0;
-      }
-      return 0;
-
-    }
-  };
-} // ycsbc
+		ret = delete_key(dbs[db_id], (void *)key.c_str(), key.size());
+		if (ret != 0) {
+			std::cerr << "I could not delete " << key << std::endl;
+			return 0;
+		}
+		return 0;
+	}
+};
+} // namespace ycsbc
 
 #endif // YCSB_C_EUTROPIA_DB_H_
