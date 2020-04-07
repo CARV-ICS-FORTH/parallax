22a9a134b05ba017808613d1407bd0bae69371b3
gesalous <gesalous@ics.forth.gr>
2020-04-07 21:39:09 +0300

    Kreon r metadata fixes

--- kreon_rdma_client/kreon_rdma_client.h
+++ kreon_rdma_client/kreon_rdma_client.h
@@ -15,8 +15,11 @@ typedef enum krc_scan_state {
 	KRC_FETCH_NEXT_BATCH,
 	KRC_ISSUE_MGET_REQ,
 	KRC_ADVANCE,
+	KRC_STOP_FILTER,
+	KRC_PREFIX_FILTER,
 	KRC_END_OF_DB,
 	KRC_BUFFER_OVERFLOW,
+	KRC_INVALID
 } krc_scan_state;
 
 typedef enum krc_seek_mode { KRC_GREATER_OR_EQUAL = 1, KRC_GREATER } krc_seek_mode;
@@ -49,6 +52,7 @@ typedef struct krc_scanner {
 	uint32_t pos;
 	uint8_t start_infinite : 2;
 	krc_seek_mode seek_mode;
+	krc_seek_mode stop_key_seek_mode;
 	uint8_t stop_infinite : 2;
 	uint8_t prefix_filter_enable : 2;
 	uint8_t is_valid : 2;
@@ -70,7 +74,7 @@ uint32_t krc_delete(uint32_t key_size, void *key);
 /*scanner API*/
 krc_scanner *krc_scan_init(uint32_t prefetch_entries, uint32_t prefetch_mem_size_hint);
 void krc_scan_set_start(krc_scanner *sc, uint32_t start_key_size, void *start_key, krc_seek_mode seek_mode);
-void krc_scan_set_stop(krc_scanner *sc, uint32_t stop_key_size, void *stop_key);
+void krc_scan_set_stop(krc_scanner *sc, uint32_t stop_key_size, void *stop_key, krc_seek_mode seek_mode);
 void krc_scan_set_prefix_filter(krc_scanner *sc, uint32_t prefix_size, void *prefix);
 void krc_scan_get_next(krc_scanner *sc);
 uint8_t krc_scan_is_valid(krc_scanner *sc);
