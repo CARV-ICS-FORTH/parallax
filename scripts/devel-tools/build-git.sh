7714a969ef4fd346db053e8e23568afe0ac13480
George Xanthakis <gxanth@ics.forth.gr>
2020-04-18 00:30:05 +0300

    [feat][doc] Add build scripts for development tools

---
+++ scripts/devel-tools/build-git.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://github.com/git/git/archive/v2.26.0.tar.gz
+tar -xf v2.26.0.tar.gz
+cd git-2.26.0
+make configure
+./configure --prefix=/archive/users/gxanth/git
+make -j 12
+make install
