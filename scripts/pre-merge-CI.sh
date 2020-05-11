6da3a236706ab88f8dffd5e78e9a12766530ffad
gxanth <gxanth@ics.forth.gr>
2020-05-11 14:33:14 +0300

    [fix] Use dockerhub image

--- scripts/pre-merge-CI.sh
+++ scripts/pre-merge-CI.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 pip install -U pip
 pip install pre-commit
-FILES=$(git diff --name-only -r "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME"..."$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME" | awk '$1=$1' ORS=' ')
-pre-commit run --files "$(printf '%s' "$FILES")"
+FILES=$(git --no-pager diff origin/"$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" --name-only)
+echo "$FILES"
+
+for i in $FILES; do
+	echo ""
+	echo "$i "
+	pre-commit run --files "$i"
+	echo ""
+done
