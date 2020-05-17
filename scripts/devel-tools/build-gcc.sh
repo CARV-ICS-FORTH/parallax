976f24b2b7de88ceecc3c8b529fc72c57022e9a3
George Xanthakis <gxanth@ics.forth.gr>
2020-05-17 23:11:14 +0300

    [refactor] Make the script generic

--- scripts/devel-tools/build-gcc.sh
+++ scripts/devel-tools/build-gcc.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-
-wget https://ftpmirror.gnu.org/gcc/gcc-9.1.0/gcc-9.1.0.tar.gz
-tar xf gcc-9.1.0.tar.gz
-cd gcc-9.1.0 || exit
+GCCVERSION=10.1.0
+mkdir temp
+cd temp || exit
+wget https://ftpmirror.gnu.org/gcc/gcc-"$GCCVERSION"/gcc-"$GCCVERSION".tar.gz
+tar xf gcc-"$GCCVERSION".tar.gz
+cd gcc-"$GCCVERSION" || exit
 contrib/download_prerequisites
-cd ..
+cd .. || exit
 mkdir build
 cd build || exit
 
-../gcc-9.1.0/configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --prefix=/archive/users/gxanth/gcc-9.1 --enable-checking=release --enable-languages=c,c++,fortran --disable-multilib --program-suffix=-9.1
+../gcc-"$GCCVERSION"/configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --prefix=/archive/users/gxanth/gcc-"$GCCVERSION" --enable-checking=release --enable-languages=c,c++,fortran --disable-multilib --program-suffix=-"$GCCVERSION"
 
 make -j 24
-
-export PATH=/archive/users/gxanth/gcc-9.1/bin:$PATH
-export LD_LIBRARY_PATH=/archive/users/gxanth/gcc-9.1/lib64:$LD_LIBRARY_PATH
+make install-strip
+export PATH=/archive/users/gxanth/gcc-"$GCCVERSION"/bin:$PATH
+export LD_LIBRARY_PATH=/archive/users/gxanth/gcc-"$GCCVERSION"/lib64:$LD_LIBRARY_PATH
