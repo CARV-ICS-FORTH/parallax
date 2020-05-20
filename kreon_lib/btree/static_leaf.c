2785d13e314bd5a0cefcbc7c7dbb39efbe4a5c51
George Xanthakis <gxanth@ics.forth.gr>
2020-05-21 00:58:48 +0300

    [refactor] Change binary search to report status for each operation
    Instead of returning the key that we will operate on, binary search
    returns a status based on the final comparison.
    
    * It retuns INSERT if the key does not already exist in the leaf.
    * It returns UPDATE if the key is found in the leaf.
    * It returns ERROR if we happen to attempt to insert in a leaf when we
    must not.

--- kreon_lib/btree/static_leaf.c
+++ kreon_lib/btree/static_leaf.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <limits.h>
 #include "static_leaf.h"
@@ -37,29 +38,30 @@ void init_static_leaf_metadata(bt_static_leaf_node *leaf)
 	retrieve_static_leaf_structures(leaf, &src);
 	/* If the bt_leaf_bitmap struct becomes a different type than an unsigned char,
 	   the UCHAR_MAX should be replaced with the maximum value the type can hold.  */
-	memset(src.bitmap, UCHAR_MAX, get_bitmap_size(level_id));
+	memset(src.bitmap, 0, get_bitmap_size(level_id));
 	memset(src.slot_array, 0, get_slot_array_size(level_id));
 	memset(src.kv_entries, 0, get_kv_entries_size(level_id));
 }
 
-uint32_t binary_search_static_leaf(bt_static_leaf_node const *leaf, struct splice *key_buf)
+struct bsearch_result binary_search_static_leaf(bt_static_leaf_node const *leaf, struct splice *key_buf)
 {
 	struct bt_static_leaf_structs src;
 	char *leaf_key_prefix, *leaf_key_buf;
-	int32_t start = 0;
-	int32_t middle = 0;
-	int32_t end = leaf->header.numberOfEntriesInNode - 1;
+	struct bsearch_result result = { 0, INSERT };
+	int32_t start = 0, middle = 0, end = leaf->header.numberOfEntriesInNode - 1;
 	const int32_t numberOfEntriesInNode = leaf->header.numberOfEntriesInNode;
 	const int32_t kv_entries = leaf_node_offsets[leaf->header.level_id].kv_entries;
 	uint32_t pos;
-	int ret;
-	int ret_case;
+	int ret, ret_case;
 	retrieve_static_leaf_structures(leaf, &src);
 
 	while (numberOfEntriesInNode > 0) {
 		middle = (start + end) / 2;
-		if (numberOfEntriesInNode > kv_entries || middle < 0 || middle >= numberOfEntriesInNode)
-			return UINT32_MAX;
+
+		if (numberOfEntriesInNode > kv_entries || middle < 0 || middle >= numberOfEntriesInNode) {
+			result.status = ERROR;
+			return result;
+		}
 
 		pos = src.slot_array[middle].index;
 		leaf_key_prefix = src.kv_entries[pos].prefix;
@@ -69,8 +71,12 @@ uint32_t binary_search_static_leaf(bt_static_leaf_node const *leaf, struct splic
 		if (ret_case == EQUAL_TO_ZERO) {
 			leaf_key_buf = (void *)(MAPPED + src.kv_entries[pos].pointer);
 			ret = _tucana_key_cmp(leaf_key_buf, key_buf, KV_FORMAT, KV_FORMAT);
-			if (ret == 0)
-				return middle;
+
+			if (ret == 0) {
+				result.middle = middle;
+				result.status = UPDATE;
+				return result;
+			}
 
 			ret_case = ret < 0 ? LESS_THAN_ZERO : GREATER_THAN_ZERO;
 		}
@@ -80,18 +86,23 @@ uint32_t binary_search_static_leaf(bt_static_leaf_node const *leaf, struct splic
 			start = middle + 1;
 			if (start > end) {
 				middle++;
-				return middle;
+				result.middle = middle;
+				result.status = INSERT;
+				return result;
 			}
 			continue;
 		case GREATER_THAN_ZERO:
 			end = middle - 1;
-			if (start > end)
-				return middle;
+			if (start > end) {
+				result.middle = middle;
+				result.status = INSERT;
+				return result;
+			}
 			continue;
 		}
 	}
 
-	return EQUAL_TO_ZERO;
+	return result;
 }
 
 void shift_slot_array(bt_static_leaf_node *leaf, uint32_t middle)
--- kreon_lib/btree/static_leaf.h
+++ kreon_lib/btree/static_leaf.h
@@ -1,4 +1,11 @@
 #include "btree.h"
+enum bsearch_status { INSERT, UPDATE, ERROR };
+
+struct bsearch_result {
+	int middle;
+	enum bsearch_status status;
+};
+
 #define LESS_THAN_ZERO -1
 #define GREATER_THAN_ZERO 1
 #define EQUAL_TO_ZERO 0
