c60d3591a01e8f10cda9b7e33ed506138545110e
Michalis Vardoulakis <mvard@ics.forth.gr>
2020-10-18 17:25:03 +0300

    [feat][test] TEST_REPLY is now the same size as TEST_REQUEST
    Fixes request allocation undo bug in throughput_bench.c

--- microbenchmarks/throughput_bench.c
+++ microbenchmarks/throughput_bench.c
@@ -47,21 +47,19 @@ krc_post_status krc_post_ping(connection_rdma *conn, int message_size, krc_post_
 		pthread_mutex_unlock(&conn->buffer_lock);
 		return -KRC_POST_OUT_OF_MEM;
 	}
-
+	/*log_info("Allocated request %p, %llu", req_header, (uint64_t)req_header - (uint64_t)conn->rdma_memory_regions->local_memory_buffer);*/
 	/*Now the reply part*/
-	rep_header = client_try_allocate_rdma_message(conn, sizeof(msg_header), TEST_REPLY);
+	rep_header = client_try_allocate_rdma_message(conn, message_size, TEST_REPLY);
 	if (!rep_header) {
-		uint32_t size;
-		if (req_header->pay_len == 0) {
-			size = MESSAGE_SEGMENT_SIZE;
-		} else {
-			size = TU_HEADER_SIZE + req_header->pay_len + req_header->padding_and_tail;
-			assert(size % MESSAGE_SEGMENT_SIZE == 0);
-		}
-		free_space_from_circular_buffer(conn->send_circular_buf, (char *)req_header, size);
+		int allocated_size = sizeof(msg_header) + req_header->pay_len + req_header->padding_and_tail;
+		/*log_info("Reply alloc failed, allocated_size = %d", allocated_size);*/
+		free_space_from_circular_buffer(conn->send_circular_buf, (char *)req_header, allocated_size);
+		conn->send_circular_buf->last_addr -= allocated_size;
+		conn->send_circular_buf->remaining_space += allocated_size;
 		pthread_mutex_unlock(&conn->buffer_lock);
 		return -KRC_POST_OUT_OF_MEM;
 	}
+	/*log_info("Allocated reply %p, %llu", rep_header, (uint64_t)rep_header - (uint64_t)conn->rdma_memory_regions->remote_memory_buffer);*/
 	pthread_mutex_unlock(&conn->buffer_lock);
 	rep_header->receive = 0;
 
@@ -92,10 +90,17 @@ static inline int krc_poll_ping_reply(krc_post_handle *handle)
 	// Check if ping reply header has arrived
 	if (handle->reply->receive != TU_RDMA_REGULAR_MSG)
 		return 0;
+	/*log_info("Got header!");*/
+	uint32_t *tail = (uint32_t *)((uint64_t)handle->reply + sizeof(struct msg_header) + handle->reply->pay_len +
+				      handle->reply->padding_and_tail - TU_TAIL_SIZE);
+	if (*tail != TU_RDMA_REGULAR_MSG)
+		return 0;
 
+	/*log_info("Got payload!");*/
 	handle->reply->receive = 0;
 	_zero_rendezvous_locations(handle->reply);
 	client_free_rpc_pair(handle->conn, handle->reply);
+	/*log_info("Freed rpc pair");*/
 	return 1;
 }
 
