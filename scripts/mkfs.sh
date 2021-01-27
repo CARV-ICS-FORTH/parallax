acd10af28f8deee45bf5c46a175283b2c0c4a8a1
Giorgos Xanthakis <gxanth@ics.forth.gr>
2021-01-27 17:44:48 +0200

    [refactor] Rename mkfs

--- scripts/mkfs.sh
+++ scripts/mkfs.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-#  sudo chown gesalous /dev/fbd
 if [ $# -ne 3 ]; then
-	echo 'Usage: mkfs.eutropia.single.sh <device name> <number of DBs> <type of volume (0 for device 1 for file)>'
+	echo 'Usage: mkfs.sh <device name> <number of DBs> <type of volume (0 for device 1 for file)>'
 	exit 1
 fi
 
-INSIDE_BUILD_DIR=../kreon_lib/mkfs.kreon
-BUILD_PATH=../build/kreon_lib/mkfs.kreon
-SYSTEM_PATH=/usr/local/bin/mkfs.kreon
+INSIDE_BUILD_DIR=../lib/mkfs.parallax
+BUILD_PATH=../build/lib/mkfs.parallax
+SYSTEM_PATH=/usr/local/bin/mkfs.parallax
 
 if [ -f "$INSIDE_BUILD_DIR" ]; then
 	echo "Executable chosen $INSIDE_BUILD_DIR"
-	MKFS_KREON=$INSIDE_BUILD_DIR
+	MKFS=$INSIDE_BUILD_DIR
 elif [ -f "$BUILD_PATH" ]; then
 	echo "Executable chosen $BUILD_PATH"
-	MKFS_KREON=$BUILD_PATH
+	MKFS=$BUILD_PATH
 else
 	echo "System Path $SYSTEM_PATH executable chosen"
-	MKFS_KREON=$SYSTEM_PATH
+	MKFS=$SYSTEM_PATH
 fi
 
 DEV_NAME=$1
@@ -45,6 +44,6 @@ echo 'Allocator size:' "${ALLOCATOR_SIZE}"
 
 for i in $(seq 0 $((DB_NUM - 1))); do
 	OFFSET=$( (${i} \* ALLOCATOR_SIZE))
-	${MKFS_KREON} "${DEV_NAME}" "${OFFSET}" "${ALLOCATOR_SIZE}" >/dev/null
+	${MKFS} "${DEV_NAME}" "${OFFSET}" "${ALLOCATOR_SIZE}" >/dev/null
 done
 exit 0
