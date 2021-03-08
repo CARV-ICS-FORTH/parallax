cef7606671a3beae34a00b01ac38cc56ffb55437
George Xanthakis <gxanth@ics.forth.gr>
2021-03-08 17:05:55 +0200

    [fix] Conflicting names in spin_loop.c

--- utilities/spin_loop.c
+++ utilities/spin_loop.c
@@ -1,22 +1,6 @@
 #include <stdint.h>
 
-int64_t get_counter(int64_t *counter);
-uint32_t _read_value(uint32_t *value_addr);
-
-void spin_loop(int64_t *counter, int64_t threashold)
-{
-	while (get_counter(counter) > threashold) {
-	}
-	return;
-}
-
-void wait_for_value(uint32_t *value_addr, uint32_t value)
-{
-	while (_read_value(value_addr) != value) {
-	}
-}
-
-uint32_t _read_value(uint32_t *value_addr)
+uint32_t read_value(uint32_t *value_addr)
 {
 	return *value_addr;
 }
@@ -24,4 +8,16 @@ uint32_t _read_value(uint32_t *value_addr)
 int64_t get_counter(int64_t *counter)
 {
 	return *counter;
+}
+
+void spin_loop(int64_t *counter, int64_t threashold)
+{
+	while (get_counter(counter) > threashold)
+		;
+}
+
+void wait_for_value(uint32_t *value_addr, uint32_t value)
+{
+	while (read_value(value_addr) != value)
+		;
 }
