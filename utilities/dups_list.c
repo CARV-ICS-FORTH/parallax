fc282566c91447a43ee1fc29ab86d8b0e549cea8
George Xanthakis <gxanth@ics.forth.gr>
2021-05-12 17:43:33 +0300

    [feat] Add dups list source

---
+++ utilities/dups_list.c
@@ -0,0 +1,79 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <log.h>
+#include "dups_list.h"
+
+struct dups_list *init_dups_list(void)
+{
+	struct dups_list *new_list = calloc(1, sizeof(struct dups_list));
+
+	if (!new_list) {
+		log_fatal("Calloc returned NULL!");
+		exit(EXIT_FAILURE);
+	}
+
+	return new_list;
+}
+
+void append_node(struct dups_list *list, uint64_t dev_offset, uint64_t kv_size)
+{
+	assert(list);
+
+	struct dups_node *new_node = calloc(1, sizeof(struct dups_node));
+
+	if (!new_node) {
+		log_fatal("Calloc returned NULL!");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->dev_offset = dev_offset;
+	new_node->kv_size = kv_size;
+
+	if (!list->head || list->head->dev_offset >= dev_offset) {
+		new_node->next = list->head;
+		list->head = new_node;
+	} else {
+		struct dups_node *curr = list->head;
+		while (curr->next && curr->next->dev_offset < dev_offset) {
+			curr = curr->next;
+		}
+		new_node->next = curr->next;
+		curr->next = new_node;
+	}
+}
+
+struct dups_node *find_element(struct dups_list *list, uint64_t dev_offset)
+{
+	assert(list);
+
+	struct dups_node *node;
+
+	for (node = list->head; node && node->dev_offset != dev_offset; node = node->next)
+		;
+
+	return node;
+}
+
+void free_dups_list(struct dups_list **list)
+{
+	assert(*list);
+	struct dups_node *temp_node = (*list)->head;
+	struct dups_node *next_node;
+
+	for (temp_node = (*list)->head; temp_node; temp_node = next_node) {
+		next_node = temp_node->next;
+		free(temp_node);
+	}
+
+	free(*list);
+	*list = NULL;
+}
+
+void print_ascending_list(struct dups_list *list)
+{
+	if (!list)
+		return;
+
+	for (struct dups_node *curr = list->head; curr; curr = curr->next)
+		log_info("print in order %llu", curr->dev_offset);
+}
---
+++ utilities/dups_list.h
@@ -0,0 +1,29 @@
+#ifndef DUPS_LIST_H_
+#define DUPS_LIST_H_
+#include <stdint.h>
+
+struct dups_node {
+	uint64_t dev_offset;
+	uint64_t kv_size;
+	struct dups_node *next;
+};
+
+struct dups_list {
+	struct dups_node *head;
+};
+
+struct dups_list *init_dups_list(void);
+
+/*Takes a device offset and appends at the end of the list.
+ *It is a checked runtime error for `list` not to be NULL.*/
+void append_node(struct dups_list *list, uint64_t dev_offset, uint64_t kv_size);
+
+struct dups_node *find_element(struct dups_list *list, uint64_t dev_offset);
+
+/*Takes a `list` initialized by `init_dups_list` and releases the memory allocated for its nodes.
+ *It is checked runtime error for `list` not to be NULL. */
+void free_dups_list(struct dups_list **list);
+
+void print_ascending_list(struct dups_list *list);
+
+#endif // DUPS_LIST_H_
