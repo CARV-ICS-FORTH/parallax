ec585f2bbdccde355dfef5bce5cf89902d638019
George Xanthakis <gxanth@ics.forth.gr>
2021-05-25 15:47:54 +0300

    [fix] Fix Fastmap bug when pages were not zero initialized

--- lib/allocator/persistent_operations.c
+++ lib/allocator/persistent_operations.c
@@ -182,15 +182,35 @@ void pr_flush_log_tail(struct db_descriptor *db_desc, struct volume_descriptor *
 	(void)volume_desc;
 	int last_tail = log_desc->curr_tail_id % LOG_TAIL_NUM_BUFS;
 	uint64_t offt_in_seg = log_desc->size % SEGMENT_SIZE;
-	uint32_t chunk_id = offt_in_seg / LOG_CHUNK_SIZE;
 	uint64_t start_offt, end_offt;
+	uint32_t chunk_id;
+	uint32_t idx = log_desc->size % SEGMENT_SIZE;
+
+	if (idx) {
+		uint32_t remaining_bytes = SEGMENT_SIZE - idx;
+		memset(&log_desc->tail[last_tail]->buf[idx], 0, remaining_bytes);
+		log_desc->size += remaining_bytes;
+	}
+
+	if (offt_in_seg == 0) {
+		chunk_id = (SEGMENT_SIZE / LOG_CHUNK_SIZE) - 1;
+		log_info("flush log tail first case");
+	} else
+		chunk_id = offt_in_seg / LOG_CHUNK_SIZE;
+
+	for (int i = chunk_id; i < SEGMENT_SIZE / LOG_CHUNK_SIZE; ++i) {
+		log_desc->tail[last_tail]->bytes_in_chunk[i] = LOG_CHUNK_SIZE;
+	}
+	log_desc->tail[last_tail]->IOs_completed_in_tail = SEGMENT_SIZE / LOG_CHUNK_SIZE;
+
 	if (chunk_id)
 		start_offt = chunk_id * LOG_CHUNK_SIZE;
 	else
 		start_offt = sizeof(struct segment_header);
 
 	ssize_t bytes_written = 0;
-	end_offt = start_offt + LOG_CHUNK_SIZE;
+	//end_offt = start_offt + LOG_CHUNK_SIZE;
+	end_offt = SEGMENT_SIZE - start_offt;
 	while (start_offt < end_offt) {
 		bytes_written = pwrite(FD, &log_desc->tail[last_tail]->buf[start_offt], end_offt - start_offt,
 				       log_desc->tail[last_tail]->dev_offt + start_offt);
