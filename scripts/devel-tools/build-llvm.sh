9d848c72254466d1f0e7f730c6642040f32bc2a1
George Xanthakis <gxanth@ics.forth.gr>
2021-07-19 23:32:40 +0300

    [version] Bump gcc/clang version to match latest stable versions

--- scripts/devel-tools/build-llvm.sh
+++ scripts/devel-tools/build-llvm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export CC=gcc-9.1
-export CXX=g++-9.1
-LLVMVERSION=10.0.0
+set -xeu
+export CC=gcc-11.1.0
+export CXX=g++-11.1.0
+LLVMVERSION=12.0.1
 mkdir llvm-project
 cd llvm-project || exit
 
