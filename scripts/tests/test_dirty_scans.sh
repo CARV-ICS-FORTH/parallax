0c4d770676446476e8ac23fc46249a01260bc8ef
George Xanthakis <gxanth@ics.forth.gr>
2022-02-05 01:51:08 +0200

    [refactor] Rename variable name in test_dirty_scans.sh

--- scripts/tests/test_dirty_scans.sh
+++ scripts/tests/test_dirty_scans.sh
@@ -5,18 +5,18 @@ declare -a KV_MIX=("s" "m" "l" "sd" "md" "ld")
 readonly FILE=/tmp/kv_store.dat
 readonly NUM_OF_KVS=4000000
 readonly SCAN_SIZE=30
-WORKLOAD="All"
+readonly WORKLOAD="All"
 
-for i in "${KV_MIX[@]}"; do
-	echo "Running workload $i back to back!"
+for curr_kv_mix in "${KV_MIX[@]}"; do
+	echo "Running workload $curr_kv_mix back to back!"
 	./mkfs.sh "$FILE" 128
-	./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="$WORKLOAD" --kv_mix="$i"
+	./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="$WORKLOAD" --kv_mix="$curr_kv_mix"
 done
 
-for i in "${KV_MIX[@]}"; do
-	echo "Running workload $i with par_close!"
+for curr_kv_mix in "${KV_MIX[@]}"; do
+	echo "Running workload $curr_kv_mix with par_close!"
 	./mkfs.sh "$FILE" 128
-	./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="Load" --kv_mix="$i"
-	./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="Get" --kv_mix="$i"
-	./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="Scan" --kv_mix="$i"
+	./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="Load" --kv_mix="$curr_kv_mix"
+	./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="Get" --kv_mix="$curr_kv_mix"
+	./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="Scan" --kv_mix="$curr_kv_mix"
 done
