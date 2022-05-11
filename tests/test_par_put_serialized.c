93f3b648705573159adef5824f99c27b4b4554bc
Giorgos Xanthakis <gxanth@ics.forth.gr>
2022-05-11 16:19:49 +0300

    [fix] Fix par_format not to free device name
    * Also, fixes test_par_put_serialized offsets

--- tests/test_par_put_serialized.c
+++ tests/test_par_put_serialized.c
@@ -1,5 +1,6 @@
 #include "arg_parser.h"
 #include <parallax.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define MAX_REGIONS 128
@@ -16,30 +17,31 @@ int main(int argc, char *argv[])
 		{ { 0, 0, 0, 0 }, "End of arguments", NULL, INTEGER }
 	};
 	unsigned options_len = (sizeof(options) / sizeof(struct wrap_option));
-	char *path = get_option(options, 1);
 	arg_parse(argc, argv, options, options_len);
 	arg_print_options(help_flag, options, options_len);
 
+	char *path = get_option(options, 1);
 	par_format(path, MAX_REGIONS);
 	par_db_options db_options = { .volume_name = path,
 				      .volume_start = 0,
 				      .volume_size = 0,
 				      .create_flag = PAR_CREATE_DB,
 				      .db_name = "serialized_par_put.db" };
-
 	par_handle handle = par_open(&db_options);
-	char serialized_key_value[1024];
+	char serialized_key_value[1024] = { 0 };
 	char *serialize_kv = serialized_key_value;
 	*(uint32_t *)serialize_kv = 10;
-	serialize_kv += 10;
+	serialize_kv += 4;
 	strcpy(serialize_kv, "abcdabcda");
 	serialize_kv += 10;
 	*(uint32_t *)serialize_kv = 1;
-	serialize_kv += 1;
+	serialize_kv += 4;
 	*serialize_kv = 'a';
 
 	if (par_put_serialized(handle, serialized_key_value) != PAR_SUCCESS)
 		return EXIT_FAILURE;
+
+	par_close(handle);
 
 	return EXIT_SUCCESS;
 }
