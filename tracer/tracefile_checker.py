bab0a6fb8eb3d1b72c0ca4cd0f58d7a6517ae590
GioStil <styl.george1@gmail.com>
2022-06-30 18:41:54 +0100

    [fix] Fix pre-commit CI Job

--- tracer/tracefile_checker.py
+++ tracer/tracefile_checker.py
@@ -2,6 +2,7 @@ import argparse
 import enum
 import sys
 
+
 class colors:
     RED = "\033[31m"
     ENDC = "\033[m"
@@ -10,6 +11,7 @@ class colors:
     BLUE = "\033[34m"
     PURPLE = "\033[35m"
     LIGHT_BLUE = "\033[36m"
+
 
 def change_color(color):
     if color == colors.RED:
@@ -25,41 +27,49 @@ def change_color(color):
 
     return color
 
+
 class Op(enum.Enum):
     PUT = 1
     GET = 2
     NONE = 3
 
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Execute tracer with appropriate parameters")
+    parser = argparse.ArgumentParser(
+        description="Execute tracer with appropriate parameters"
+    )
     parser.add_argument("file", type=str, help="tracefile to be parsed")
     return parser.parse_args()
 
+
 def get_op(line_list):
-    if(line_list[0] == "PUT"):
+    if line_list[0] == "PUT":
         return Op.PUT
-    elif(line_list[0] == "GET"):
+    elif line_list[0] == "GET":
         return Op.GET
     else:
         return Op.NONE
 
+
 def validate_get_op_format(line_list):
     key_size = int(line_list[1])
     key = line_list[2]
-    if(key_size != len(key)):
+    if key_size != len(key):
         print(colors.RED + "key " + key + " is not valid" + colors.ENDC)
         print("Exiting..")
         sys.exit(1)
+
 
 def validate_put_op_format(line_list):
     key_size = int(line_list[1])
-    key = line_list[2];
+    key = line_list[2]
     value_size = int(line_list[3])
 
-    if(key_size != len(key)):
+    if key_size != len(key):
         print(colors.RED + "key " + key + " is not valid" + colors.ENDC)
         print("Exiting..")
         sys.exit(1)
+
 
 # validate that the line follows a certain pattern
 # pattern is:
@@ -68,45 +78,55 @@ def validate_put_op_format(line_list):
 def validate_line(line):
     str_line_list = line.split()
     op = get_op(str_line_list)
-    if(op == Op.PUT):
+    if op == Op.PUT:
         validate_put_op_format(str_line_list)
-    elif(op == Op.GET):
+    elif op == Op.GET:
         validate_get_op_format(str_line_list)
     else:
-        print(colors.RED + "Invalid operation: " + str_line_list[0] + " in tracefile" + colors.ENDC)
+        print(
+            colors.RED
+            + "Invalid operation: "
+            + str_line_list[0]
+            + " in tracefile"
+            + colors.ENDC
+        )
         print("Exiting..")
         sys.exit(1)
 
+
 def count_lines(filename):
-    tracefile = open(filename,'r')
-    line =tracefile.readline()
-    count =1
-    while(line):
+    tracefile = open(filename, "r")
+    line = tracefile.readline()
+    count = 1
+    while line:
         count += 1
         line = tracefile.readline()
     tracefile.close()
     return count
 
+
 def validate_tracefile(args):
     filename = args.file
     number_of_lines = count_lines(filename)
 
-    tracefile = open(filename,'r')
+    tracefile = open(filename, "r")
     line = tracefile.readline()
     line_count = 1
     while line:
-        #last line may be incomplete if this is a tracefile from a bug
-        if(line_count != number_of_lines - 1):
+        # last line may be incomplete if this is a tracefile from a bug
+        if line_count != number_of_lines - 1:
             validate_line(line)
         line = tracefile.readline()
         line_count += 1
 
     tracefile.close()
 
+
 def main():
     args = parse_arguments()
     validate_tracefile(args)
     print(colors.GREEN + "tracefile is valid!" + colors.ENDC)
+
 
 if __name__ == "__main__":
     main()
