eb95a41a44abc6c6fafc4fa5503dd7b782f9a40e
Giorgos Stylianakis <geostyl@ics.forth.gr>
2022-09-11 18:01:19 +0300

    [refactor] Minor refactor on SERIALIZE_KEY

--- lib/btree/btree.h
+++ lib/btree/btree.h
@@ -510,7 +510,7 @@ lock_table *_find_position(const lock_table **table, node_header *node);
 #define VALUE_SIZE_OFFSET(KEY_SIZE, KEY) (sizeof(uint32_t) + KEY_SIZE + KEY)
 #define SERIALIZE_KEY(buf, key, key_size) \
 	*(uint32_t *)buf = key_size;      \
-	memcpy(buf + 4, key, key_size)
+	memcpy(buf + sizeof(uint32_t), key, key_size)
 #define KV_MAX_SIZE (4096 + 8)
 #define likely(x) __builtin_expect((x), 1)
 #define unlikely(x) __builtin_expect((x), 0)
