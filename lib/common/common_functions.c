15768b8723808d6dd8fbdaf47e50382a3db4cdca
Giorgos Stylianakis <geostyl@ics.forth.gr>
2022-10-17 00:28:38 +0300

    [refactor] Make common functions inline

--- lib/common/common_functions.c
+++ lib/common/common_functions.c
@@ -1,73 +1,70 @@
 #include "common_functions.h"
 #include <assert.h>
 
-uint32_t get_key_size(struct splice *kv)
+inline uint32_t get_key_size(struct splice *kv)
 {
 	return kv->key_size;
 }
 
-uint32_t get_key_size_with_metadata(struct splice *kv)
+inline uint32_t get_key_size_with_metadata(struct splice *kv)
 {
 	return sizeof(kv->key_size) + kv->key_size;
 }
 
-uint32_t get_value_size(struct splice *kv)
+inline uint32_t get_value_size(struct splice *kv)
 {
 	return kv->value_size;
 }
 
-uint32_t get_value_size_with_metadata(struct splice *kv)
+inline uint32_t get_value_size_with_metadata(struct splice *kv)
 {
 	return sizeof(kv->value_size) + kv->value_size;
 }
 
 // Returns the size of a kv_formated kv
 // TODO (geostyl) FIXME add proper documentation
-uint32_t get_kv_metadata_size(void)
+inline uint32_t get_kv_metadata_size(void)
 {
 	uint32_t kv_metadata_size = sizeof(struct splice);
-	// size of key_size + value_size of splice
-	// this assert will fail if key_size or value_size change in splice.
-	assert(kv_metadata_size == sizeof(uint32_t) + sizeof(uint32_t));
-
 	return kv_metadata_size;
 }
 
-uint32_t get_kv_size(struct splice *kv)
+inline uint32_t get_kv_size(struct splice *kv)
 {
 	return kv->key_size + kv->value_size + get_kv_metadata_size();
 }
 
-void set_key_size(struct splice *kv, uint32_t key_size)
+inline void set_key_size(struct splice *kv, uint32_t key_size)
 {
 	kv->key_size = key_size;
 }
 
-void set_value_size(struct splice *kv, uint32_t value_size)
+inline void set_value_size(struct splice *kv, uint32_t value_size)
 {
 	kv->value_size = value_size;
 }
 
-char *get_key_offset_in_kv(struct splice *kv)
+inline char *get_key_offset_in_kv(struct splice *kv)
 {
 	return kv->data;
 }
-char *get_value_offset_in_kv(struct splice *kv, uint32_t key_size)
+
+inline char *get_value_offset_in_kv(struct splice *kv, uint32_t key_size)
 {
 	return kv->data + key_size;
 }
 
-uint32_t get_key_size_kv_seperated(struct kv_seperation_splice *kv)
+inline uint32_t get_key_size_kv_seperated(struct kv_seperation_splice *kv)
 {
 	return get_key_size((struct splice *)kv->dev_offt);
 }
 
-uint32_t get_value_size_kv_seperated(struct kv_seperation_splice *kv)
+inline uint32_t get_value_size_kv_seperated(struct kv_seperation_splice *kv)
 {
 	return get_value_size((struct splice *)kv->dev_offt);
 }
 
-uint32_t get_kv_size_kv_seperated(struct kv_seperation_splice *kv)
+inline uint32_t get_kv_size_kv_seperated(struct kv_seperation_splice *kv)
 {
 	return get_kv_size((struct splice *)kv->dev_offt);
 }
