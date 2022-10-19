9a995060b00950a53ca9d466d2a0a30a9bd01edd
Giorgos Stylianakis <geostyl@ics.forth.gr>
2022-10-19 18:49:37 +0300

    [feat] Add documentation for lsn

--- lib/btree/lsn.h
+++ lib/btree/lsn.h
@@ -7,9 +7,28 @@ struct lsn {
 	int64_t id;
 };
 
+/**
+ * returns the size of lsn struct
+ * */
 size_t get_lsn_size(void);
+/**
+ * returns the difference of left_lsn - right_lsn value
+ * @param left: a ptr to struct lsn
+ * @param right: a ptr to struct lsn
+ * */
 int64_t compare_lsns(struct lsn *left, struct lsn *right);
+/**
+ * returns the ticket(id) of the lsn
+ * @param lsn: a ptr to a struct lsn
+ * */
 int64_t get_lsn_id(struct lsn *lsn);
+/**
+ * atomically increase the lsn ticket(id) by one and returns the previus lsn
+ * @param lsn: a ptr to a struct lsn
+ * */
 struct lsn increase_lsn(struct lsn *lsn);
+/**
+ * reset the lsn ticket(id) to 0
+ * */
 void reset_lsn(struct lsn *lsn);
 #endif
