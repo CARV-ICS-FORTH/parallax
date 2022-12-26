849f830f2e5950ed06807d280072f231dd2b772a
Giorgos Xanthakis <gxanth@ics.forth.gr>
2022-12-26 17:39:07 +0200

    [refactor] Refactor test_dirty_scans to autocreate db file

--- scripts/tests/test_dirty_scans.sh
+++ scripts/tests/test_dirty_scans.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 set -exvpipefail
+ARGS=""
+for item in "${@:1}"; do
+	ARGS+=" $item"
+done
 
 KV_MIX="$1"
-readonly FILEPATH="$NVME0"/"$CI_JOB_ID"
-readonly FILE="$FILEPATH"/kv_store.dat
+if [[ -z "${NVME0}" ]]; then
+	readonly FILEPATH="$NVME0"/"$CI_JOB_ID"
+	readonly FILE="$FILEPATH"/kv_store.dat
+else
+	FILE=$(echo "$ARGS" | grep -o "\--file=[^ ]*" | grep -oE '=\S+')
+	FILE="${FILE:1}"
+	DIRECTORY_NAME=$(dirname "$FILE")
+	if [[ "$DIRECTORY_NAME" != "." ]]; then
+		mkdir -p "$DIRECTORY_NAME"
+	fi
+fi
+
 readonly NUM_OF_KVS=4000000
 readonly SCAN_SIZE=30
 
+fallocate -l 16G "$FILE"
 echo "Running workload $KV_MIX with par_close!"
 mkdir -p "$FILEPATH"
-./mkfs.sh "$FILE" 128
 ./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="Load" --kv_mix="$KV_MIX"
 ./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="Get" --kv_mix="$KV_MIX"
 ./test_dirty_scans --file="$FILE" --num_of_kvs="$NUM_OF_KVS" --scan_size="$SCAN_SIZE" --workload="Scan" --kv_mix="$KV_MIX"
