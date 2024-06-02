21e39b3d2d53401219d9000de1dff95b9ee836a1
Giorgos Xanthakis <gxanth@ics.forth.gr>
2024-06-02 11:07:53 +0300

    [fix] Fix premerge script

--- scripts/pre-merge-CI.sh
+++ scripts/pre-merge-CI.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+python -m venv venv
+source venv/bin/activate
 pip install gitlint
 FILES=$(git --no-pager diff "origin/$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" --name-only)
 echo "$FILES"
