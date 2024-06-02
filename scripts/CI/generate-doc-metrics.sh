dee51b8579638a6cfb0dfbf68d1ddbcf9db2c77f
Giorgos Xanthakis <gxanth@ics.forth.gr>
2024-06-02 11:33:33 +0300

    [fix] Fix generate doc metrics script

--- scripts/CI/generate-doc-metrics.sh
+++ scripts/CI/generate-doc-metrics.sh
@@ -3,14 +3,15 @@ set -xe
 
 # Clone master branch in /tmp and generate its documentation
 cd /tmp
-git clone https://gitlab-ci-token:"${CI_JOB_TOKEN}"@carvgit.ics.forth.gr/storage/parallax.git
+git clone https://gitlab-ci-token:"${CI_JOB_TOKEN}"@carvgit.ics.forth.gr/gxanth/"${CI_PROJECT_NAME}".git parallax
 cd parallax
 doxygen Doxyfile
 
 # Generate documentation for current branch
-cd /builds/storage/parallax/
+cd /builds/gxanth/"${CI_PROJECT_NAME}"/
 doxygen Doxyfile
-
+python -m venv venv
+source venv/bin/activate
 pip install --no-cache-dir coverxygen
 # Compare documentation coverage for the two branches
-python3 scripts/CI/coverdocs.py "$CI_COMMIT_BRANCH" /builds/storage/parallax "$CI_DEFAULT_BRANCH" /tmp/parallax
+python3 scripts/CI/coverdocs.py "$CI_COMMIT_BRANCH" /builds/gxanth/"${CI_PROJECT_NAME}" "$CI_DEFAULT_BRANCH" /tmp/parallax
