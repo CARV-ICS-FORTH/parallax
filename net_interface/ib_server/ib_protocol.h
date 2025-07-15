5dab5994903c2273ceb93cc4e4d286025cf9b095
Nikolaos Lasithiotakis <nikosl@ics.forth.gr>
2025-07-15 12:26:52 +0000

    [feat] Add __attribute__((packed)) to protocol header struct

--- net_interface/ib_server/ib_protocol.h
+++ net_interface/ib_server/ib_protocol.h
@@ -3,7 +3,7 @@
 
 enum op_code { OP_OPEN = 0, OP_CLOSE = 1, OP_WRITE = 2, OP_READ = 3, OP_DEL = 4, OP_SCAN = 5 };
 
-struct protocol_header {
+struct __attribute__((packed)) protocol_header {
 	uint64_t virtual_address;
 	enum op_code op;
 	uint8_t db_id;
