b2cef00afd82099bb75c36211bb0d45007e1ee1b
Nikolaos Lasithiotakis <nikosl@ics.forth.gr>
2025-07-21 10:54:03 +0000

    [refactor] Move ib_client_ctx struct to header for reuse

---
+++ net_interface/ib_server/ib_client_ctx.h
@@ -0,0 +1,15 @@
+#include <rdma/rdma_cma.h>
+
+struct ib_client_ctx {
+	struct rdma_event_channel *ec;
+	struct rdma_cm_id *id;
+	struct ibv_pd *pd;
+	struct ibv_cq *cq;
+	struct ibv_mr *mr;
+	struct ibv_qp *qp;
+	struct ibv_comp_channel *comp_channel;
+	void *buf;
+	struct server_handle *server_handle;
+};
+
+struct ib_client_ctx *get_default_client_ctx(void);
