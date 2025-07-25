3ac8dd29add70587296a86e87edc964aec5856eb
Nikolaos Lasithiotakis <nikosl@ics.forth.gr>
2025-07-25 08:41:07 +0000

    [test] Add basic test covering open, put, get, and close operations

--- net_interface/ib_server/tests/ib_api_test.c
+++ net_interface/ib_server/tests/ib_api_test.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
 #include "parallax.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    par_db_options db_options = { .volume_name = (char *)"/tmp/par",
+int main(void)
+{
+	par_db_options db_options = { .volume_name = (char *)"/tmp/par",
 				      .db_name = "test_db",
 				      .create_flag = PAR_CREATE_DB,
 				      .options = par_get_default_options() };
@@ -14,7 +17,7 @@ int main() {
 	const char *error_message = NULL;
 	par_handle handle = par_open(&db_options, &error_message);
 
-    if (error_message) {
+	if (error_message) {
 		printf("Parallax says: %s\n", error_message);
 	}
 
@@ -22,5 +25,39 @@ int main() {
 		printf("Error upon opening the DB, error %s\n", error_message);
 	}
 
-    return 0;
+	const char *keyStr = "test";
+	const char *valueStr = "data";
+
+	struct par_value value = {
+		.val_buffer_size = strlen(valueStr) + 1,
+		.val_size = strlen(valueStr) + 1,
+		.val_buffer = malloc(strlen(valueStr) + 1),
+	};
+	if (!value.val_buffer) {
+		fprintf(stderr, "Memory allocation failed\n");
+		return 1;
+	}
+	memcpy(value.val_buffer, valueStr, value.val_size);
+	struct par_key_value kv = {
+		.k = {
+			.size = strlen(keyStr) + 1,
+			.data = keyStr,
+		},
+		.v = value,
+	};
+	const char *error_msg = NULL;
+	par_put(handle, &kv, &error_msg);
+
+	struct par_key key = {
+		.size = strlen(keyStr) + 1,
+		.data = keyStr,
+	};
+	struct par_value getValue;
+	par_get(handle, &key, &getValue, &error_msg);
+
+	free(value.val_buffer);
+
+	par_close(handle);
+
+	return 0;
 }
