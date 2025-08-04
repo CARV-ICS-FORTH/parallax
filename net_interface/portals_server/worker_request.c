252a629e41577f266d25c35bfc84049c6a8f5997
Nikolaos Lasithiotakis <nikosl@ics.forth.gr>
2025-08-04 10:16:31 +0000

    [refactor] Reuse worker_request for InfiniBand backend

--- net_interface/portals_server/worker_request.c
+++ net_interface/portals_server/worker_request.c
@@ -1,18 +1,24 @@
-#include "worker_request.h"
 #include "portals.h"
+#include "worker_request.h"
+#ifdef USE_PORTALS
 #include "portals4.h"
+#endif
 #include <stdlib.h>
 
 struct portals_worker_request {
+#ifdef USE_PORTALS
 	ptl_process_t initiator;
+#endif
 	void *start;
 	void *user_ptr;
 };
 
+#ifdef USE_PORTALS
 ptl_process_t portals_worker_get_initiator(const struct portals_worker_request *req)
 {
 	return req->initiator;
 }
+#endif
 
 void *portals_worker_get_start(const struct portals_worker_request *req)
 {
@@ -24,6 +30,7 @@ void *portals_worker_get_user_ptr(const struct portals_worker_request *req)
 	return req->user_ptr;
 }
 
+#ifdef USE_PORTALS
 struct portals_worker_request *portals_worker_create_req(ptl_event_t event)
 {
 	struct portals_worker_request *req = calloc(1U, sizeof(struct portals_worker_request));
@@ -32,3 +39,12 @@ struct portals_worker_request *portals_worker_create_req(ptl_event_t event)
 	req->user_ptr = event.user_ptr;
 	return req;
 }
+#elif USE_INFINIBAND
+struct portals_worker_request *portals_worker_create_req(void *buf)
+{
+	struct portals_worker_request *req = calloc(1U, sizeof(struct portals_worker_request));
+	req->start = buf;
+	req->user_ptr = buf;
+	return req;
+}
+#endif
--- net_interface/portals_server/worker_request.h
+++ net_interface/portals_server/worker_request.h
@@ -1,8 +1,10 @@
 #ifndef WORKER_REQUEST_H
 #define WORKER_REQUEST_H
+
 #ifdef USE_PORTALS
 #include "portals4.h"
 #endif
+
 struct portals_worker_request;
 
 /* Header for worker request obj*/
@@ -13,11 +15,14 @@ struct portals_worker_request *portals_worker_create_req(ptl_event_t event);
 
 ptl_process_t portals_worker_get_initiator(const struct portals_worker_request *req);
 
+#elif USE_INFINIBAND
+
+struct portals_worker_request *portals_worker_create_req(void *buf);
+
 #endif
 
 void *portals_worker_get_start(const struct portals_worker_request *req);
 
 void *portals_worker_get_user_ptr(const struct portals_worker_request *req);
-
 
 #endif
